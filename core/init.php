08436ef04bd284d255671e2e636244b629d1a092
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-04 22:40:04 +0100

    Database class.

--- core/init.php
+++ core/init.php
@@ -4,9 +4,9 @@ session_start();
 $GLOBALS['config'] = array(
     'mysql' => array(
         'host' => '127.0.0.1',
-        'username' => 'root',
+        'username' => 'brendan',
         'password' => '',
-        'db' => 'password-manager'
+        'db' => 'password_manager'
     ),
     'remember' => array(
         'cookie_name' => 'hash',
