29f26372a81b0dbddda69b0bac4426139fdc1a2c
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-05 20:21:40 +0100

    DB updates added

--- classes/DB.php
+++ classes/DB.php
@@ -68,6 +68,51 @@ class DB
         return false;
     }
 
+    public function insert($table, $fields = array())
+    {
+        $keys = array_keys($fields);
+        $values = '';
+        $x = 1;
+
+        foreach ($fields as $field) {
+            $values .= '?';
+            if ($x < count($fields)) {
+                $values .= ', ';
+            }
+            $x++;
+        }
+
+        $sql = "INSERT INTO users (`" . implode('`, `', $keys) . "`) VALUES ({$values})";
+
+        if (!$this->query($sql, $fields)->error()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function update($table, $id, $fields)
+    {
+        $set = '';
+        $x = 1;
+
+        foreach ($fields as $name => $value) {
+            $set .= "{$name} = ?";
+
+            if ($x < count($fields)) {
+                $set .= ', ';
+            }
+            $x++;
+        }
+
+        $sql = "UPDATE {$table} SET {$set} WHERE id = {$id}";
+
+        if (!$this->query($sql, $fields)->error()) {
+            return true;
+        }
+        return false;
+
+    }
+
     public function get($table, $where)
     {
         return $this->action('SELECT *', $table, $where);
