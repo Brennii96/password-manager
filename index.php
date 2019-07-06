7f3ca697fe85ee8d4b339298ffc730c36b0e77b5
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-06 22:48:05 +0100

    Changed title

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $user = new User();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Password Manager</title>
     <?php include 'includes/icons.php'; ?>
 </head>
 <body>
