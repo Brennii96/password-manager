e86214593c45fb626a5e215a4836eb10ae34a3b1
Brennii96 <brendan.oneill96@hotmail.co.uk>
2019-07-06 19:46:51 +0100

    User can now logout and be redirected to index.php

--- classes/User.php
+++ classes/User.php
@@ -62,6 +62,11 @@ class User
         return false;
     }
 
+    public function logout()
+    {
+        Session::delete($this->_sessionName);
+    }
+
     public function data()
     {
         return $this->_data;
